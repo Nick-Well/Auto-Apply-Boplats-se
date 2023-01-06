7b596f0d6a6f1850384a688a795b87fc6372d469
Nick <nick2018w@gmail.com>
2023-01-06 18:06:29 +0100

    the start of an automatic apartment searcher for boplats.
    on going:
    *login file
    *adding filter settings
    *login secure
    *some backend shits

--- main.py
+++ main.py
@@ -2,50 +2,67 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
+# from cryptography.fernet import Fernet
 import re
 import getpass
 
 # Set the username and password to empty strings
 
-userdata = ["name","pass","consist"]
+
 options = Options()
 options.headless = True
-# Create a new Firefox webdriver
-driver = webdriver.Firefox(options=options)
+# Create a new Firefox webdriver options=options
+driver = webdriver.Firefox()
 
-def startUp():
+"""key = Fernet.generate_key()
+fernet = Fernet(key)"""
+
+userdata = ["name", "pass", "consist"]
+
+
+def start_up():
+    global userdata
     print("\nWelcome this script is made to make it easier to look for apartment without actually "
           "having to go in on the web page.\n")
     print("Also the settings are ment to be persistent so you can add this as a automatic service\n"
           "i would recommend to put it on 2am every day as new apartments get added att 1am\n")
 
-    forLogin = open("userdata.txt", "r")
-    print(forLogin.readlines()[2])
-    forLogin.close()
+    try:
+        for_login = open("userdata.txt", "r")
+        for_login_ram = for_login.readlines()[2]
+        for_login.close()
+    except IndexError:
+        for_login_ram = "n"
+    except FileNotFoundError:
+        for_login_ram = "n"
 
-    if forLogin == "n":
+    if for_login_ram == "n":
         print("I will not check your login. So wright it correctly or it wont work")
         userdata[0] = input("Personnummer eller E-post:\n")
         # add encryption
         userdata[1] = getpass.getpass(prompt="Lösenord:\n")
-        userdata[2] = input("Spara användardata: Y/n\n")or "y"
+        userdata[2] = input("Spara användardata: Y/n\n") or "y"
         if userdata[2] == "y":
             print("Makes the file")
 
-            userFile = open("userdata.txt", "wt")
-            userFile.writelines(userdata[0])
-            userFile.writelines(userdata[1])
-            userFile.writelines(userdata[2])
-            userFile.close()
+            user_file = open("userdata.txt", "wt")
+            user_file.writelines(userdata[0] + "\n")
+            user_file.writelines(userdata[1] + "\n")
+            user_file.writelines(userdata[2] + "\n")
+            user_file.close()
 
             # open and read the file after the appending:
-            userFile = open("userdata.txt", "r")
-            print(userFile.readlines())
-            userFile.close()
+            user_file = open("userdata.txt", "r")
+            print(user_file.readlines())
+            user_file.close()
             print("sparat")
-
-    print(userdata)
+    else:
+        for_login = open("userdata.txt", "r")
+        userdata = [re.sub(r'\n', '', i) for i in for_login.readlines()]
+        for_login.close()
+    login()
 
 
 def login():
@@ -61,11 +78,24 @@ def login():
 
     # Find the login button and click it
     driver.find_element(By.XPATH, "//*[@id=\"loginform-submit\"]").click()
+    if check_login():
+        if check_counter():
+            look_for_apartment()
+    else:
+        for_login_ram = "n"
+        print("login failed name or password was wrong")
+        start_up()
 
-    if checkCounter() is True:
-        lookForApartment()
 
-def checkCounter():
+def check_login():
+    try:
+        driver.find_element(By.XPATH, "/html/body/div[1]/div/div[5]/div[1]/div[1]/span[1]")
+    except NoSuchElementException:
+        return False
+    return True
+
+
+def check_counter():
     bol = False
     driver.get('https://nya.boplats.se/minsida/ansokta')
     soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -75,18 +105,19 @@ def checkCounter():
     clean = soup.find("span", class_="application-count")
 
     # Print the contents of the p element
-    count = int(re.sub('[\W_]+', " ", clean.text))
-    print("Mängder sökta som är fortfarande sökande " + count)
-    if count != 5:
+    count = re.sub('[\W_]+', "", clean.text)
+    print("Mängder sökta lägenheter " + count)
+    if int(count) != 5:
         bol = True
 
     return bol
 
-def lookForApartment():
-    #make an array for changing the settings for the filter
+
+def look_for_apartment():
     driver.get('https://nya.boplats.se/sok?types=1hand&objecttype=alla&rent=7000&squaremeters=40&rooms=3')
     print("look")
+    # make an array for changing the settings for the filter
 
 
-startUp()
+start_up()
 driver.quit()
